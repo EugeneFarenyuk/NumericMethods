5a4228f84ea940df768308178c5bcf0393b2841d
Eugene Farenyuk <eugene.farenyuk@gmail.com>
2016-12-14 17:46:03 -0500

    test line added.

--- src/com/github/EugeneFarenyuk/NumericMethods/CalcPiUsingSeriesForLoop.java
+++ src/com/github/EugeneFarenyuk/NumericMethods/CalcPiUsingSeriesForLoop.java
@@ -8,6 +8,7 @@ public class CalcPiUsingSeriesForLoop {
         double Pi=0;
         // sequence {1,3,5,7,...} has n-th term = 2*n-1.
         // to alternate plus and minus use Math.pow(-1.0, i+1.0).
+        // test line.
         for(int i=1; i<=1000; i++)  Pi = Pi + 1.0/(2.0*i-1) * Math.pow(-1.0, i+1.0);
         Pi = 4*Pi;
         System.out.println(Pi);
